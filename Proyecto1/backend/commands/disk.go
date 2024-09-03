c99798003095396c3e9d6911050f8199f7fa75e2
Dennis2441 <dennis2441@gmail.com>
2024-09-03 13:13:14 -0400

    ver

--- Proyecto1/backend/commands/disk.go
+++ Proyecto1/backend/commands/disk.go
@@ -8,6 +8,7 @@ import (
 	"math/rand"
 	"os"
 	"path"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -329,19 +330,285 @@ func Rdisk(pathvalor string) error {
 	}
 	return nil
 }
-func MountPartition(pathvalor string, name string) string {
+func MountPartition(pathValor string, name string) string {
 	var respuesta string
-
-	//abrir archivo
-	archivo, err := os.OpenFile(pathvalor, os.O_RDWR, 0664)
+	//Abir el archivo
+	archivo, err := os.OpenFile(pathValor, os.O_RDWR, 0664)
 	if err != nil {
-
 		fmt.Println("Error: No se pudo abrir el archivo")
 		respuesta += "Error: No se pudo abrir el archivo\n"
 		return respuesta
 	}
-
 	defer archivo.Close()
-	return respuesta
+	//Leer el MBR
+	var disk structs.MBR
+	archivo.Seek(0, 0)
+	err = binary.Read(archivo, binary.LittleEndian, &disk)
+	if err != nil {
+		fmt.Println("Error: No se pudo leer el archivo")
+		respuesta += "Error: No se pudo leer el archivo\n"
+		return respuesta
+	}
+	//Verificar MBR sea valido
+	if disk.Mbr_tamano == 0 {
+		fmt.Println("Error: Disco no valido")
+		respuesta += "Error: Disco no valido\n"
+		return respuesta
+	}
+	//Verificar si la particion existe
+	indiceParticion := 0
 
+	if strings.Contains(string(disk.Mbr_partition_1.Part_name[:]), name) {
+		indiceParticion = 1
+	} else if strings.Contains(string(disk.Mbr_partition_2.Part_name[:]), name) {
+		indiceParticion = 2
+	} else if strings.Contains(string(disk.Mbr_partition_3.Part_name[:]), name) {
+		indiceParticion = 3
+	} else if strings.Contains(string(disk.Mbr_partition_4.Part_name[:]), name) {
+		indiceParticion = 4
+	}
+
+	if indiceParticion == 0 {
+		fmt.Println("Error: La particion no existe")
+		respuesta += "Error: La particion no existe\n"
+		return respuesta
+	}
+
+	for i := 0; i < len(particionesMontadas); i++ {
+		if particionesMontadas[i].Path == pathValor && particionesMontadas[i].Name == name {
+			fmt.Println("Error: La particion ya esta montada")
+			respuesta += "Error: La particion ya esta montada\n"
+			return respuesta
+		}
+	}
+
+	if indiceParticion == 1 {
+		particion := disk.Mbr_partition_1
+
+		//Montar la particion
+		var particionMontada Mount
+		particionMontada.Path = pathValor
+		particionMontada.Name = name
+		particionMontada.Part_type = particion.Part_type
+
+		/* Últimos dos dígitos del Carnet + Número de Partición + LetraEjemplo: carnet = 202401234
+		Id´s = 341A, 341B, 341C, 342A, 343A
+		Al montar la partición debe validar:
+		● Si es una partición del mismo disco se incrementa en uno el número
+		de partición.
+		● Si la partición es de otro disco se debe colocar la letra siguiente en el
+		orden del abecedario (A,B,C,D, etc) y volver a iniciar en uno el número
+		de partición al utilizar la siguiente letra.
+		● El número de particiones se obtien */
+
+		contador := 1
+		for i := 0; i < len(particionesMontadas); i++ {
+			if particionesMontadas[i].Path == pathValor {
+				contador++
+			}
+		}
+
+		pathExiste := false
+		for i := 0; i < len(pathsParticiones); i++ {
+			if pathsParticiones[i] == pathValor {
+				pathExiste = true
+				break
+			}
+		}
+		if !pathExiste {
+			pathsParticiones = append(pathsParticiones, pathValor)
+		}
+
+		alfabeto := []byte{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+
+		letra := alfabeto[len(pathsParticiones)-1]
+
+		particionMontada.Id = "45" + strconv.Itoa(contador) + string(letra)
+		particionMontada.Size = particion.Part_size
+		particionMontada.Start = particion.Part_start
+		particionesMontadas = append(particionesMontadas, particionMontada)
+		fmt.Println("Particion montada correctamente " + name + " con id " + particionMontada.Id)
+		respuesta += "Particion montada correctamente " + name + " con id " + particionMontada.Id + "\n"
+
+		for i := 0; i < len(particionesMontadas); i++ {
+			respuesta += "Id: " + particionesMontadas[i].Id + " Path: " + particionesMontadas[i].Path + " Name: " + particionesMontadas[i].Name + " Part_type: " + string(particionesMontadas[i].Part_type[:]) + " Start: " + strconv.Itoa(int(particionesMontadas[i].Start)) + " Size: " + strconv.Itoa(int(particionesMontadas[i].Size)) + "\n"
+		}
+
+		disk.Mbr_partition_1.Part_status = [1]byte{'1'}
+		disk.Mbr_partition_1.Part_correlative = int32(contador)
+		archivo.Seek(0, 0)
+		binary.Write(archivo, binary.LittleEndian, &disk)
+
+		return respuesta
+	} else if indiceParticion == 2 {
+		particion := disk.Mbr_partition_2
+
+		//Montar la particion
+		var particionMontada Mount
+		particionMontada.Path = pathValor
+		particionMontada.Name = name
+		particionMontada.Part_type = particion.Part_type
+
+		/* Últimos dos dígitos del Carnet + Número de Partición + LetraEjemplo: carnet = 202401234
+		Id´s = 341A, 341B, 341C, 342A, 343A
+		Al montar la partición debe validar:
+		● Si es una partición del mismo disco se incrementa en uno el número
+		de partición.
+		● Si la partición es de otro disco se debe colocar la letra siguiente en el
+		orden del abecedario (A,B,C,D, etc) y volver a iniciar en uno el número
+		de partición al utilizar la siguiente letra.
+		● El número de particiones se obtien */
+
+		contador := 1
+		for i := 0; i < len(particionesMontadas); i++ {
+			if particionesMontadas[i].Path == pathValor {
+				contador++
+			}
+		}
+
+		pathExiste := false
+		for i := 0; i < len(pathsParticiones); i++ {
+			if pathsParticiones[i] == pathValor {
+				pathExiste = true
+				break
+			}
+		}
+		if !pathExiste {
+			pathsParticiones = append(pathsParticiones, pathValor)
+		}
+
+		alfabeto := []byte{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+
+		letra := alfabeto[len(pathsParticiones)-1]
+
+		particionMontada.Id = "45" + strconv.Itoa(contador) + string(letra)
+		particionMontada.Size = particion.Part_size
+		particionMontada.Start = particion.Part_start
+		particionesMontadas = append(particionesMontadas, particionMontada)
+		fmt.Println("Particion montada correctamente " + name + " con id " + particionMontada.Id)
+		respuesta += "Particion montada correctamente " + name + " con id " + particionMontada.Id + "\n"
+
+		for i := 0; i < len(particionesMontadas); i++ {
+			respuesta += "Id: " + particionesMontadas[i].Id + " Path: " + particionesMontadas[i].Path + " Name: " + particionesMontadas[i].Name + " Part_type: " + string(particionesMontadas[i].Part_type[:]) + " Start: " + strconv.Itoa(int(particionesMontadas[i].Start)) + " Size: " + strconv.Itoa(int(particionesMontadas[i].Size)) + "\n"
+		}
+		disk.Mbr_partition_2.Part_status = [1]byte{'1'}
+		disk.Mbr_partition_2.Part_correlative = int32(contador)
+		archivo.Seek(0, 0)
+		binary.Write(archivo, binary.LittleEndian, &disk)
+
+		return respuesta
+	} else if indiceParticion == 3 {
+		particion := disk.Mbr_partition_3
+
+		//Montar la particion
+		var particionMontada Mount
+		particionMontada.Path = pathValor
+		particionMontada.Name = name
+		particionMontada.Part_type = particion.Part_type
+
+		/* Últimos dos dígitos del Carnet + Número de Partición + LetraEjemplo: carnet = 202401234
+		Id´s = 341A, 341B, 341C, 342A, 343A
+		Al montar la partición debe validar:
+		● Si es una partición del mismo disco se incrementa en uno el número
+		de partición.
+		● Si la partición es de otro disco se debe colocar la letra siguiente en el
+		orden del abecedario (A,B,C,D, etc) y volver a iniciar en uno el número
+		de partición al utilizar la siguiente letra.
+		● El número de particiones se obtien */
+
+		contador := 1
+		for i := 0; i < len(particionesMontadas); i++ {
+			if particionesMontadas[i].Path == pathValor {
+				contador++
+			}
+		}
+
+		pathExiste := false
+		for i := 0; i < len(pathsParticiones); i++ {
+			if pathsParticiones[i] == pathValor {
+				pathExiste = true
+				break
+			}
+		}
+		if !pathExiste {
+			pathsParticiones = append(pathsParticiones, pathValor)
+		}
+
+		alfabeto := []byte{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+
+		letra := alfabeto[len(pathsParticiones)-1]
+
+		particionMontada.Id = "45" + strconv.Itoa(contador) + string(letra)
+		particionMontada.Size = particion.Part_size
+		particionMontada.Start = particion.Part_start
+		particionesMontadas = append(particionesMontadas, particionMontada)
+		fmt.Println("Particion montada correctamente " + name + " con id " + particionMontada.Id)
+		respuesta += "Particion montada correctamente " + name + " con id " + particionMontada.Id + "\n"
+
+		for i := 0; i < len(particionesMontadas); i++ {
+			respuesta += "Id: " + particionesMontadas[i].Id + " Path: " + particionesMontadas[i].Path + " Name: " + particionesMontadas[i].Name + " Part_type: " + string(particionesMontadas[i].Part_type[:]) + " Start: " + strconv.Itoa(int(particionesMontadas[i].Start)) + " Size: " + strconv.Itoa(int(particionesMontadas[i].Size)) + "\n"
+		}
+		disk.Mbr_partition_3.Part_status = [1]byte{'1'}
+		disk.Mbr_partition_3.Part_correlative = int32(contador)
+		archivo.Seek(0, 0)
+		binary.Write(archivo, binary.LittleEndian, &disk)
+		return respuesta
+	} else if indiceParticion == 4 {
+		particion := disk.Mbr_partition_4
+
+		//Montar la particion
+		var particionMontada Mount
+		particionMontada.Path = pathValor
+		particionMontada.Name = name
+		particionMontada.Part_type = particion.Part_type
+
+		/* Últimos dos dígitos del Carnet + Número de Partición + LetraEjemplo: carnet = 202401234
+		Id´s = 341A, 341B, 341C, 342A, 343A
+		Al montar la partición debe validar:
+		● Si es una partición del mismo disco se incrementa en uno el número
+		de partición.
+		● Si la partición es de otro disco se debe colocar la letra siguiente en el
+		orden del abecedario (A,B,C,D, etc) y volver a iniciar en uno el número
+		de partición al utilizar la siguiente letra.
+		● El número de particiones se obtien */
+
+		contador := 1
+		for i := 0; i < len(particionesMontadas); i++ {
+			if particionesMontadas[i].Path == pathValor {
+				contador++
+			}
+		}
+
+		pathExiste := false
+		for i := 0; i < len(pathsParticiones); i++ {
+			if pathsParticiones[i] == pathValor {
+				pathExiste = true
+				break
+			}
+		}
+		if !pathExiste {
+			pathsParticiones = append(pathsParticiones, pathValor)
+		}
+
+		alfabeto := []byte{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+
+		letra := alfabeto[len(pathsParticiones)-1]
+
+		particionMontada.Id = "45" + strconv.Itoa(contador) + string(letra)
+		particionMontada.Size = particion.Part_size
+		particionMontada.Start = particion.Part_start
+		particionesMontadas = append(particionesMontadas, particionMontada)
+		fmt.Println("Particion montada correctamente " + name + " con id " + particionMontada.Id)
+		respuesta += "Particion montada correctamente " + name + " con id " + particionMontada.Id + "\n"
+
+		for i := 0; i < len(particionesMontadas); i++ {
+			respuesta += "Id: " + particionesMontadas[i].Id + " Path: " + particionesMontadas[i].Path + " Name: " + particionesMontadas[i].Name + " Part_type: " + string(particionesMontadas[i].Part_type[:]) + " Start: " + strconv.Itoa(int(particionesMontadas[i].Start)) + " Size: " + strconv.Itoa(int(particionesMontadas[i].Size)) + "\n"
+		}
+		disk.Mbr_partition_4.Part_status = [1]byte{'1'}
+		disk.Mbr_partition_4.Part_correlative = int32(contador)
+		archivo.Seek(0, 0)
+		binary.Write(archivo, binary.LittleEndian, &disk)
+		return respuesta
+	}
+	return respuesta
 }
